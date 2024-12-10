529801a4b19286012e095e3141a4a3eca6d62877
Melisa Bajramović <115657992+Melisa-os@users.noreply.github.com>
2024-12-10 23:06:23 +0100

    Add files via upload

---
+++ parsiranje_teksta/parsiranje teksta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace MyCsApp
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CityOfBirth { get; set; }
+
+        public Person(string firstName, string lastName, string cityOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            CityOfBirth = cityOfBirth;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{FirstName} {LastName} {CityOfBirth}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            string text = "John.Davidson/Belgrade Michael.Barton/Krakow Ivan.Perkinson/Moscow";
+
+            string[] personData = text.Split(' ');
+
+            Person[] people = new Person[personData.Length];
+
+            for (int i = 0; i < personData.Length; i++)
+            {
+                string[] parts = personData[i].Split(new char[] { '.', '/' });
+
+                string firstName = parts[0];
+                string lastName = parts[1];
+                string cityOfBirth = parts[2];
+
+                people[i] = new Person(firstName, lastName, cityOfBirth);
+            }
+
+            foreach (Person person in people)
+            {
+                person.DisplayInfo();
+            }
+        }
+    }
+
+}
