3f6a3fa029aa1c1ccff118a8278261d40ee53e4c
Melisa Bajramović <115657992+Melisa-os@users.noreply.github.com>
2024-12-10 20:00:44 +0100

    Add files via upload

---
+++ nizPOzNeg_gh/niPOzNeg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 12, 23, -22, -765, 43, 545, -4, -55, 43, 12, 351, -999, -87 };
+        int[] poziNiz = new int[array.Length];
+        int[] negNiz = new int[array.Length];
+        int pozIndeks = 0;
+        int negIndeks = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+            {
+                poziNiz[pozIndeks] = array[i];
+                pozIndeks++;
+            }
+            else if (array[i] < 0)
+            {
+                negNiz[negIndeks] = array[i];
+                negIndeks++;
+            }
+            
+        }
+        Console.WriteLine("Pozitivni brojevi:");
+        for (int i = 0; i < pozIndeks; i++)
+        {
+            Console.Write(poziNiz[i] + " ");
+        }
+
+        Console.WriteLine("\nNegativni brojevi:");
+        for (int i = 0; i < negIndeks; i++)
+        {
+            Console.Write(negNiz[i] + " ");
+        }
+    }
+}
---
+++ zapremina_valjka_gh/zapremina_valjka.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        const double Pi = 3.14;
+
+
+        double poluprecnik = 10;
+        double visina = 50;
+
+
+        double zapremina = Pi * Math.Pow(poluprecnik, 2) * visina;
+
+
+        Console.WriteLine("Zapremina valjka sa poluprečnikom {0} i visinom {1} je {2}.", poluprecnik, visina, zapremina);
+    }
+}
+
